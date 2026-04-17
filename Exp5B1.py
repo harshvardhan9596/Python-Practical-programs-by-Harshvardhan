3963e3c1f27efc5b1e08967938103c96a718e288
Harshvardhan010100 <harshvardhan9596@gmail.com>
2026-04-17 13:09:00 +0530

    Add files via upload

---
+++ Exp5B1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 17 13:05:31 2026
+
+@author: Harshvardhan Uday Gaikwad
+"""
+set1 = set(map(int, input("Enter elements of first set (space-separated): ").split()))
+set2 = set(map(int, input("Enter elements of second set (space-separated): ").split()))
+union_set = set1 | set2
+intersection_set = set1 & set2
+print("Union of the sets:", union_set)
+print("Intersection of the sets:", intersection_set)
