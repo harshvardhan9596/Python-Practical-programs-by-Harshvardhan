2ce01ad47105e43e63c2b86df74eacb86d24bd13
Harshvardhan010100 <harshvardhan9596@gmail.com>
2026-04-30 15:27:57 +0530

    Add files via upload

---
+++ Act3B.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 30 15:25:51 2026
+
+@author: Harshvardhan Gaikwad
+"""
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are allowed to watch the movie ")
+else:
+    print("Sorry, you are not eligible ")
