269aaee4b03c15aa830adf665e7ae6eef84b9ee9
Harshvardhan010100 <harshvardhan9596@gmail.com>
2026-04-30 15:22:49 +0530

    Add files via upload

---
+++ Act3A.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 30 15:20:24 2026
+
+@author: Harshvardhan gaikwad
+"""
+speed = float(input("Enter vehicle speed (km/h): "))
+
+if speed > 60:
+    print("Overspeeding! You have to pay a fine ")
+else:
+    print("Speed is within limit ")
