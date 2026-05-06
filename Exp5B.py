c5b53a294c42703c283bddab0b205273b21374d3
Harshvardhan010100 <harshvardhan9596@gmail.com>
2026-05-06 14:14:18 +0530

    Update Exp5B.py

--- Exp5B.py
+++ Exp5B.py
@@ -2,7 +2,7 @@
 """
 Created on Fri Apr 17 13:05:31 2026
 
-@author: Harshvardhan Uday Gaikwad
+@author: Ashish Gaikwad
 """
 set1 = set(map(int, input("Enter elements of first set (space-separated): ").split()))
 set2 = set(map(int, input("Enter elements of second set (space-separated): ").split()))
